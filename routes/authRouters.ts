1ecf956512a863ea7402273e681f6bc0de584d8c
Alexey Yegorov <ye.alexey@gmail.com>
2022-05-12 14:21:09 +0300

    remove files

--- routes/authRouters.ts
+++
@@ -1,7 +0,0 @@
-import * as express from 'express';
-import * as authController from '../controllers/authController';
-
-export const authRouter = express.Router();
-
-authRouter.post('/signup', authController.signUp);
-authRouter.post('/signin', authController.signIn);
--- routes/userRouters.ts
+++
@@ -1,8 +0,0 @@
-import * as express from 'express';
-import * as userController from '../controllers/userController';
-
-export const userRouter = express.Router();
-
-userRouter.get('/:id', userController.getOne);
-userRouter.patch('/:id', userController.updateUser);
-userRouter.delete('/:id', userController.deleteUser);
