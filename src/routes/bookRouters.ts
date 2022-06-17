fa1ae904f14cf7c387742984a3e2a6a223846a4a
Alexey Yegorov <ye.alexey@gmail.com>
2022-06-17 10:53:10 +0300

    add validation schemas for book routers

--- src/routes/bookRouters.ts
+++ src/routes/bookRouters.ts
@@ -4,17 +4,15 @@ import getAllBooks from '../controllers/bookControllers/getAllBooks';
 import getOneBook from '../controllers/bookControllers/getOneBook';
 import getAllGenres from '../controllers/bookControllers/getAllGenres';
 import validator from '../utils/validator/bookValidatorSchemas';
-import searchForValue from '../controllers/bookControllers/searchForValue';
 import setRating from '../controllers/bookControllers/setRating';
 import addComment from '../controllers/bookControllers/addComment';
 import getRecommendations from '../controllers/bookControllers/getRecommendations';
 
 export const bookRouter = express.Router();
 
-bookRouter.get('/all', getAllBooks);
+bookRouter.get('/all', createValidatorMiddleware(validator.getAllBooks), getAllBooks);
 bookRouter.get('/genres', getAllGenres);
-bookRouter.get('/search', searchForValue);
-bookRouter.post('/rate', setRating);
-bookRouter.post('/addComment', addComment);
+bookRouter.post('/rate', createValidatorMiddleware(validator.setRating), setRating);
+bookRouter.post('/addComment', createValidatorMiddleware(validator.addComment), addComment);
 bookRouter.get('/recommendations', getRecommendations);
 bookRouter.get('/:id', createValidatorMiddleware(validator.getOneBook), getOneBook);
--- src/utils/validator/bookValidatorSchemas.ts
+++ src/utils/validator/bookValidatorSchemas.ts
@@ -1,6 +1,20 @@
 import * as yup from 'yup';
 
 const isNumberRegExp = /^[0-9]+$/;
+const isRatingValueRegExp = /^[0-5]+$/;
+
+const getAllBooks = yup.object().shape({
+  query: yup.object().shape({
+    page: yup.string().matches(isNumberRegExp, 'Bad page number'),
+    limit: yup.string().matches(isNumberRegExp, 'Bad limit number'),
+    genres: yup.string(),
+    priceFrom: yup.string().matches(isNumberRegExp, 'Wrong price value'),
+    priceTo: yup.string().matches(isNumberRegExp, 'Wrong price value'),
+    order: yup.string().matches(/(price|title|author|averageRate|dateOfIssue)/),
+    orderDir: yup.string().matches(/(ASC|DESC)/),
+    value: yup.string(),
+  }).noUnknown(true, 'wrong query params'),
+});
 
 const getOneBook = yup.object().shape({
   params: yup.object().shape({
@@ -8,6 +22,25 @@ const getOneBook = yup.object().shape({
   }),
 });
 
+const addComment = yup.object().shape({
+  body: yup.object().shape({
+    book_id: yup.string().required(),
+    user_id: yup.string().required(),
+    text: yup.string().trim().min(1).required(),
+  }).noUnknown(true, 'Bad request'),
+});
+
+const setRating = yup.object().shape({
+  body: yup.object().shape({
+    book_id: yup.string().required(),
+    user_id: yup.string().required(),
+    rating: yup.string().matches(isRatingValueRegExp).required(),
+  }).noUnknown(true, 'Bad request'),
+});
+
 export default {
+  getAllBooks,
   getOneBook,
+  addComment,
+  setRating,
 };
